4a135ee31608a479f9ab892afeb5c6d0ea7f6d00
dewantara1356 <62783851+dewantara1356@users.noreply.github.com>
2020-08-25 23:30:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const axios = require("axios")
 const urls = ["https://bot-indoraya.glitch.me"]
+const urls = ["https://bot-command-.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
